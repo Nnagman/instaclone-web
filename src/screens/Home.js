30705440ccb24b0ca7fa0ad6a31692dd37f2a7e6
nnagm <nnagman@gmail.com>
2021-06-18 14:11:37 +0900

    8.6 Reactive Variables

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+function Home({setIsLoggedIn}) {
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={() => setIsLoggedIn(false)}>Log out now!</button>
+        </div>
+    );
+}
+
+export default Home;
