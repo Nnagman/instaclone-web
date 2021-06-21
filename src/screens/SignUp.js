56c460525567055531e69eabc9e54dd7a34e7e6b
nnagm <nnagman@gmail.com>
2021-06-21 15:10:02 +0900

    9.12 Create Account

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,8 @@
+import {gql, useMutation} from "@apollo/client";
 import {faInstagram} from "@fortawesome/free-brands-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useForm} from "react-hook-form";
+import {useHistory} from "react-router-dom";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -22,7 +25,54 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+    mutation createAccount(
+        $firstName: String!
+        $lastName: String
+        $username: String!
+        $email: String!
+        $password: String!
+    ) {
+        createAccount(
+            firstName: $firstName
+            lastName: $lastName
+            username: $username
+            email: $email
+            password: $password
+        ) {
+            ok
+            error
+        }
+    }
+`;
+
 function SignUp() {
+    const history = useHistory();
+    const onCompleted = (data) => {
+        const {
+            createAccount: {ok, error},
+        } = data;
+        if (!ok) {
+            return;
+        }
+        history.push(routes.home);
+    };
+    const [createAccount, {loading}] = useMutation(CREATE_ACCOUNT_MUTATION, {
+        onCompleted,
+    });
+    const {register, handleSubmit, formState} = useForm({
+        mode: "onChange",
+    });
+    const onSubmitValid = (data) => {
+        if (loading) {
+            return;
+        }
+        createAccount({
+            variables: {
+                ...data,
+            },
+        });
+    };
     return (
         <AuthLayout>
             <PageTitle title="Sign up"/>
@@ -33,12 +83,37 @@ function SignUp() {
                         Sign up to see photos and videos from your friends.
                     </Subtitle>
                 </HeaderContainer>
-                <form>
-                    <Input type="text" placeholder="Name"/>
-                    <Input type="text" placeholder="Email"/>
-                    <Input type="text" placeholder="Username"/>
-                    <Input type="password" placeholder="Password"/>
-                    <Button type="submit" value="Sign up"/>
+                <form onSubmit={handleSubmit(onSubmitValid)}>
+                    <Input
+                        {...register('firstName', {required: "First Name is required.",})}
+                        type="text"
+                        placeholder="First Name"
+                    />
+                    <Input
+                        {...register('lastName')}
+                        type="text"
+                        placeholder="Last Name"
+                    />
+                    <Input
+                        {...register('email', {required: "Email is required.",})}
+                        type="text"
+                        placeholder="Email"
+                    />
+                    <Input
+                        {...register('username', {required: "Username is required.",})}
+                        type="text"
+                        placeholder="Username"
+                    />
+                    <Input
+                        {...register('password', {required: "Password is required.",})}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <Button
+                        type="submit"
+                        value={loading ? "Loading..." : "Sign up"}
+                        disabled={!formState.isValid || loading}
+                    />
                 </form>
             </FormBox>
             <BottomBox cta="Have an account?" linkText="Log in" link={routes.home}/>
